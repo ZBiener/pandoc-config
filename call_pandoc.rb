2af684d105f44aa96b8bb9d80b314c42c706a339
Zvi Biener <zbiener@gmail.com>
2020-12-30 21:50:54 -0500

    0.02

---
+++ call_pandoc.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'rake'
+require 'open3'
+
+## Define standard options
+metadata_filename = "/Users/zvb1/.pandoc/defaults/userinfo.yaml"
+
+hash = {
+    "syllabus_html" 	=>   ["-d defaults -d scholar-html -d syllabus-html", "html"],
+    "syllabus_pdf"  	=>   ["-d defaults -d syllabus-latex -d pdf-xelatex --quiet", "pdf"],
+    "UC_letterhead_pdf"	=>   ["-d defaults -d UC-letterhead-latex -d pdf-xelatex --quiet", "pdf"],
+    "letter_pdf"	=>      ["-C -d letter -d pdf-xelatex --quiet", "pdf"],
+}
+
+## Match the hash key with the user input, to find the required set of options
+options, extension = hash.fetch($*[0])
+
+output_filename = File.join(File.dirname($*[1]), File.basename($*[1])).ext(extension)
+
+## Construct command
+
+command = "pandoc --metadata-file=\"#{metadata_filename}\" #{options}  --output \"#{output_filename}\" \"#{$*[1]}\""
+
+## Run command
+puts command
+
+output = ""
+error = ""
+status = 0
+Open3.popen3(command) do |stdin, stdout, stderr, thread|
+    output << stdout.read
+    error << stderr.read
+    status = thread.value.exitstatus
+end
+
+## Parse output
+if status==0
+	puts output
+	system %{open "#{output_filename}"}
+else
+    puts error
+    exit 1
+end
